ecffdd1ddd48b634a3450d2b2a68e50d09c9eb87
ALEXANDER_WHITFIELD <awhi0036@student.monash.edu>
2024-03-27 14:41:47 +1100

    new project

---
+++ Project 2 Bar Codes/main.cpp
@@ -0,0 +1,16 @@
+/*
+Created by: Alex Whitfield and Vinay Panicker
+TRC 3500 Project 1: Calculating Blob Statistics   
+*/
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <cmath>
+#include <tuple>
+//
+
+int main(){
+
+
+    return 0;
+}
